6130d748f16e3a246608d695414922ead3968044
Nic Tamura <nictamura@gmail.com>
2019-03-28 21:33:06 -0700

    remove container css, container hoc

---
+++ src/components/Common/Contain.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import withStyles from 'react-jss'
+
+const Contain = ({ classes, children }) => {
+  return (
+    <div {...classes} className={classes.container}>
+      {children}
+    </div>
+  )
+}
+
+const styles = {
+  container: {
+    paddingRight: 15,
+    paddingLeft: 15,
+    marginRight: 'auto',
+    marginLeft: 'auto',
+    boxSizing: 'border-box',
+  },
+  '@media screen and (min-width: 768px)': {
+    container: {
+      width: 750,
+    },
+  },
+  '@media screen and (min-width: 992px)': {
+    container: {
+      width: 970,
+    },
+  },
+  '@media screen and (min-width: 1200px)': {
+    container: {
+      width: 1170,
+    },
+  },
+}
+export default withStyles(styles)(Contain)
