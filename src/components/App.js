005de6d61b1494debd1ce838158e9d4f33e1f85e
Nic Tamura <nictamura@gmail.com>
2019-04-02 10:11:45 -0700

    Add better scrollbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react'
 import withStyles from 'react-jss'
-import Navbar from 'components/Navigation/Navbar'
 import { Switch, Route } from 'react-router-dom'
+import { Scrollbars } from 'react-custom-scrollbars'
+import Navbar from 'components/Navigation/Navbar'
+import Footer from 'components/Navigation/Footer'
 import Main from 'components/Views/Main'
 import About from 'components/Views/About'
 import Portfolio from 'components/Views/Portfolio'
 
 function App({ classes }) {
   return (
-    <div id="app" className={classes.app}>
-      <Navbar />
-      <div className={classes.flex}>
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/about" component={About} />
-        </Switch>
+    <Scrollbars autoHide autoHideTimeout={100}>
+      <div id="app" className={classes.app}>
+        <Navbar />
+        <div className={classes.flex}>
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+        <Footer />
       </div>
-      {/* <Footer> */}
-      <div>this is footer</div>
-      {/* </Footer> */}
-    </div>
+    </Scrollbars>
   )
 }
 
