2ed007c9c9f3e9badebc279b1d8efc0fafd0be1e
Nic Tamura <nictamura@gmail.com>
2019-04-04 11:29:14 -0700

    remove scroll libs, fix conflict issues with RR and scroll

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import withStyles from 'react-jss'
 import { Switch, Route } from 'react-router-dom'
-import { Scrollbars } from 'react-custom-scrollbars'
+import Scrollbars from 'react-scrollbars-custom'
 import Navbar from 'components/Navigation/Navbar'
 import Footer from 'components/Navigation/Footer'
 import Main from 'components/Views/Main'
@@ -9,29 +9,41 @@ import About from 'components/Views/About'
 import Portfolio from 'components/Views/Portfolio'
 
 function App({ classes }) {
+  useEffect(() => {
+    const elements = document.querySelectorAll('div:not([class]):not([id])')
+    Array.from(elements, item => item.classList.add('retroClass'))
+    // react router creates empty div that conflicts with flex styles throughout app. Required to retroactively add styles in css file
+  }, [])
+
   return (
-    <Scrollbars
-      id="app"
-      autoHide
-      autoHideTimeout={100}
-      className={classes.scroll}
-    >
-      <Navbar />
-      <div className={classes.flex}>
-        <Switch>
-          <Route exact path="/" component={Main} />
-          <Route exact path="/portfolio" component={Portfolio} />
-          <Route exact path="/about" component={About} />
-        </Switch>
-      </div>
-      <Footer />
-    </Scrollbars>
+    <div className={classes.root}>
+      <Scrollbars
+        removeTracksWhenNotUsed
+        // removeTrackYWhenNotUsed
+        noScrollX
+        contentProps={{ className: classes.scroll }}
+      >
+        <Navbar />
+        <div className={classes.body}>
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/about" component={About} primary={false} />
+          </Switch>
+        </div>
+        <Footer />
+      </Scrollbars>
+    </div>
   )
 }
 
 const styles = {
-  flex: {
-    flexGrow: 1,
+  root: {
+    flex: 1,
+  },
+  scroll: {
+    display: 'flex',
+    flexDirection: 'column',
   },
 }
 
