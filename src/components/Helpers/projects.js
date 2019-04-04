d593dacb9cd6f4f1f1fe3c804a493a910c273f40
Nic Tamura <nictamura@gmail.com>
2019-04-04 14:44:00 -0700

    projects js file

---
+++ src/components/Helpers/projects.js
@@ -0,0 +1,28 @@
+const projects = [
+  {
+    title: 'Scenarion.io',
+    tags: 'React, Redux, Material-UI, Firebase, Netlify',
+    summary:
+      'An SPA built on React, utilizing a custom Material-UI theme. I had worked together with the lead designer to bring her mockups to life. This was my first time using Redux in production and was quite the learning experience.',
+    url: 'https://scenarion.io',
+    image: '',
+  },
+  {
+    title: 'Mini-Crypto',
+    tags: 'React, React Hooks, Firebase, JSS, API, Mobile, LocalStorage',
+    summary:
+      'A cryptocurrency tracker aimed for mobile devices. This app quickly allows you to bring up the coin prices that you care about by favoriting it.',
+    url: 'https://mini-crypto.firebaseapp.com/',
+    image: '',
+  },
+  {
+    title: 'Dashvotes',
+    tags: 'React, Firebase, Firestore, Recharts, PWA',
+    summary:
+      'DashVotes is a mobile PWA which allows users to create and vote on existing polls. The votes will be collected and shown in a visual representation of the results. ',
+    url: 'https://dashvotes.firebaseapp.com/',
+    image: '',
+  },
+]
+
+export default projects
