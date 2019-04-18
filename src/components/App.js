7919feef7dbff874fa2c4c5b677df688d9058209
Nic Tamura <nictamura@gmail.com>
2019-04-18 11:26:42 -0700

    projects page, preemptive deploy

--- src/components/App.js
+++ src/components/App.js
@@ -27,7 +27,7 @@ function App({ classes }) {
         <div className={classes.body}>
           <Switch>
             <Route exact path="/" component={Main} />
-            <Route exact path="/portfolio" component={NotReady} />
+            <Route exact path="/portfolio" component={Portfolio} />
             <Route path="/portfolio/:id" component={NotReady} />
             <Route exact path="/about" component={About} />
             <Route component={NotFound} />
--- src/components/Views/Portfolio/Container.jsx
+++ src/components/Views/Portfolio/Container.jsx
@@ -1,30 +1,80 @@
 import React from 'react'
 import withStyles from 'react-jss'
-import Contain from 'components/Common/Contain'
+import { Link } from 'react-router-dom'
 import HeroSm from 'components/Common/HeroSm'
+import projects from 'components/Helpers/projects'
 import Heading from 'components/Common/Heading'
+import Contain from 'components/Common/Contain'
 
-function Container({ classes }) {
+function Portfolio({ classes }) {
   return (
-    <section className={classes.root}>
-      <Contain>
-        <HeroSm title="portfolio" />
-        <Heading text="recommendations" />
-        <p>here are some recommendations from people</p>
-        <p>here are some recommendations from people</p>
-        <p>here are some recommendations from people</p>
-        <p>here are some recommendations from people</p>
-
-        <Heading text="work" />
-        <p>Here are some of my latest projects</p>
-      </Contain>
-    </section>
+    <Contain>
+      <HeroSm title="portfolio" />
+      {/* <Heading text="recommendations" /> */}
+      <section>
+        <Heading text="portfolio" />
+        <div className={classes.grid}>
+          {projects.map(item => (
+            <div className={classes.cell}>
+              <img
+                src={item.preview}
+                className={classes.preview}
+                alt="preview"
+              />
+              <Link to={item.slug}>
+                <div className={classes.overlay}>
+                  <div className={classes.overlayTextContainer}>
+                    <h1 className={classes.title}>{item.title}</h1>
+                    <h3 className={classes.sub}>{item.type}</h3>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </section>
+    </Contain>
   )
 }
 
 const styles = {
-  root: {
-    flex: 1,
+  grid: {
+    display: 'flex',
+    '@media (max-width:630px)': {
+      flexDirection: 'column',
+    },
+  },
+  cell: {
+    flex: '1 1 auto',
+    position: 'relative',
+    margin: 16,
+    alignSelf: 'center',
+  },
+  preview: {
+    width: '100%',
+    height: 'auto',
+    maxWidth: 450,
+  },
+  overlay: {
+    backgroundColor: 'rgba(0,0,0,0.9)',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    left: 0,
+    opacity: 0,
+    transition: 'opacity .3s ease-in-out',
+    position: 'absolute',
+    '&:hover': {
+      opacity: 1,
+    },
+  },
+  overlayTextContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    textAlign: 'center',
+    height: '100%',
+    transition: 'opacity .3s ease-in-out',
   },
 }
-export default withStyles(styles)(Container)
+export default withStyles(styles)(Portfolio)
